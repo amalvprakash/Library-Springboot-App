b31e3893287a343bdc413ba5356d8b17ae398f53
Amal <amalprakash2025@gmail.com>
2022-12-06 08:46:06 +0530

    added routings

--- src/main/java/com/nest/LibraryappBackend/controller/LibraryController.java
+++ src/main/java/com/nest/LibraryappBackend/controller/LibraryController.java
@@ -1,5 +1,6 @@
 package com.nest.LibraryappBackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,35 @@ public class LibraryController {
     @PostMapping("/addBooks")
     public String AddBooks(){
         return "Add books";
+    }
+
+    @PostMapping("/issueBooks")
+    public String IssueBooks(){
+        return "Issue books";
+    }
+
+    @PostMapping("/deleteBook")
+    public String DeleteBook(){
+        return "Delete books";
+    }
+
+    @PostMapping("/adduser")
+    public String AddUser(){
+        return "Add User";
+    }
+
+    @PostMapping("/editUser")
+    public String EditUSer(){
+        return "Edit User";
+    }
+
+    @PostMapping("/editBook")
+    public String EditBook(){
+        return "Edit Book";
+    }
+
+    @GetMapping("/viewallbooks")
+    public String ViewAllBooks(){
+        return "View All Books";
     }
 }
